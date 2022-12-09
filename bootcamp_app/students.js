47b012e64e057b4fe382b793c521fbb0a3951eb8
Arnold Mesa Jr <jayrmesa@gmail.com>
2022-12-08 20:34:54 -0700

    updated student.js to use parameterized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -19,6 +19,7 @@ LIMIT $2;
 
 const cohortName = process.argv[2];
 const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
 const values = [`%${cohortName}%`, limit];
 
 pool.query(queryString, values)
